39b52efc1f94f47df965144c2286ac20a8434e54
JFSR00 <juanfsr00@gmail.com>
2020-05-13 19:13:52 +0200

    La clase Pedido_Articulo ha sido implementada en su totalidad, falta hacer las pruebas y corregir sus errores.

--- P3/pedido-articulo.cpp
+++ P3/pedido-articulo.cpp
@@ -1,4 +1,5 @@
 #include <iomanip>
+#include <utility>
 #include "pedido-articulo.hpp"
 
 // ---------------| Clase LineaPedido |---------------
@@ -14,3 +15,50 @@ void Pedido_Articulo::pedir(Pedido& p, Articulo& a, double pr, unsigned c){
 	PedArt[&p].insert(std::make_pair(&a,LineaPedido(pr,c)));
 	ArtPed[&a].insert(std::make_pair(&p,LineaPedido(pr,c)));
 }
+
+std::ostream& operator <<(std::ostream& os, Pedido_Articulo::ItemsPedido& itm_){
+	double total_=0;
+	os<<"PVP\tCantidad\tArtículo\n==================================================================\n";
+	for(auto i: itm_){
+		os<<i.second<<"\t["<<i.first->referencia()<<"] \""<<i.first->titulo()<<"\"\n";
+		total_+=(i.second.cantidad()*i.second.precio_venta());
+	}
+	os<<"==================================================================\nTotal\t"
+	<<std::fixed<<std::setprecision(2)<<total_<<" €";
+	return os;
+}
+
+std::ostream& operator <<(std::ostream& os, Pedido_Articulo::Pedidos& ped_){
+	unsigned count_=0, cantTotal_=0;
+	double total_=0;
+	std::ostream aux_os;
+	for(auto i: ped_){
+		aux_os<<i.second<<i.first->fecha()<<std::endl;
+		cantTotal_+=i.second.cantidad();
+		total_+=i.second.precio_venta();
+		count_++;
+	}
+	os<<"[Pedidos: "<<count_<<"]\n=================================================================="
+	<<"PVP\tCantidad\tFecha de venta"<<"==================================================================\n"
+	<<aux_os<<"=================================================================="<<std::fixed<<std::setprecision(2)
+	<<total_<<" €\t"<<cantTotal_;
+	return os;
+}
+
+std::ostream& Pedido_Articulo::mostrarDetallePedidos(std::ostream& os){
+	double total_=0;
+	for(auto i: PedArt){
+		os<<"Pedido núm. "<<i.first->numero()<<"\nCliente: "<<i.first->tarjeta()->titular()->nombre()<<"\t\t"
+		<<i.first->fecha()<<std::endl;
+		total_+=i.first->total();
+	}
+	os<<"\nTOTAL VENTAS\t\t"<<std::fixed<<std::setprecision(2)<<total_<<" €";
+	return os;
+}
+std::ostream& Pedido_Articulo::mostrarVentasArticulos(std::ostream& os){
+	for(auto i: ArtPed){
+		os<<"Ventas de ["<<i.first->referencia()<<"] \""<<i.first->titulo()<<"\"\n"<<i.second<<std::endl;
+	}
+	return os;
+}
+
--- P3/pedido-articulo.hpp
+++ P3/pedido-articulo.hpp
@@ -56,6 +56,9 @@ public:
 	ItemsPedido detalle(Pedido&) const;
 	Pedidos ventas(Articulo&) const;
 
+	std::ostream& mostrarDetallePedidos(std::ostream&);
+	std::ostream& mostrarVentasArticulos(std::ostream&);
+
 private:
 	std::map<Pedido*, ItemsPedido, OrdenaPedidos> PedArt;
 	std::map<Articulo*, Pedidos, OrdenaArticulos> ArtPed;
@@ -68,51 +71,7 @@ inline void Pedido_Articulo::pedir(Articulo& a,Pedido& p, double pr, unsigned c)
 inline Pedido_Articulo::ItemsPedido Pedido_Articulo::detalle(Pedido& p) const{return PedArt.find(&p)->second;}
 inline Pedido_Articulo::Pedidos Pedido_Articulo::ventas(Articulo& a) const{return ArtPed.find(&a)->second;}
 
-/*
-Se sobrecargará el operador de inserción en flujo de salida << para los tipos Pedido_
-Articulo::ItemsPedido y Pedido_Articulo::Pedidos. El primero, además de los detalles
-del pedido, mostrará el importe total y el número de ejemplares del pedido. El segundo
-mostrará precio, cantidad y fecha de cada venta y el importe total de las ventas del
-artículo. Ejemplo de salida de los detalles de un pedido:
+std::ostream& operator <<(std::ostream&, Pedido_Articulo::ItemsPedido&);
+std::ostream& operator <<(std::ostream&, Pedido_Articulo::Pedidos&);
 
-PVP Cantidad Artículo
-==================================================================
-35,20 € 2 [100] "Programación Orientada a Objetos"
-29,95 € 1 [110] "Fundamentos de C++"
-==================================================================
-Total 100,35 €
-
-Ejemplo de salida de pedidos:
-
-[Pedidos: 3]
-==================================================================
-PVP Cantidad Fecha de venta
-==================================================================
-39,95 € 2 miércoles 19 de abril de 2017
-6
-34,90 € 1 jueves 20 de abril de 2017
-34,90 € 1 lunes 5 de abril de 2010
-==================================================================
-149,70 € 4
-
-Un método llamado mostrarDetallePedidos imprimirá en el flujo de salida proporcionado
-el detalle de todos los pedidos realizados hasta la fecha, así como el importe de todas
-las ventas. Ejemplo:
-
-Pedido núm. 1
-Cliente: lucas Fecha: viernes 10 de marzo de 2017
-
-detalle de ese pedido, como en ejemplo anterior
-Resto de los pedidos ...
-
-TOTAL VENTAS 981,60 €
-
-El método mostrarVentasArticulos visualizará en el flujo de salida proporcionado todas
-las ventas agrupadas por artículos. Ejemplo:
-
-Ventas de [110] "Fundamentos de C++"
-
-pedidos de ese artículo, como en ejemplo anterior
-Resto de los artículos vendidos ...
- */
 #endif /* PEDIDO_ARTICULO_HPP_ */
